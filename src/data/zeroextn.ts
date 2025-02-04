8e53dc244eca23e5b791e7bcebabbaa8c2fae89c
mmjee <root@git.maharshi.ninja>
2025-02-04 08:44:20 +0530

    Prevent copying if possible

--- src/data/zeroextn.ts
+++ src/data/zeroextn.ts
@@ -1,8 +1,16 @@
 import { Bytes } from "../types";
 
-export function zeroExtendBufToGivenSize(buf: Bytes, size: number): Buffer {
+export function convertToBufferIfNecessary(buf: Bytes) {
+  return Buffer.isBuffer(buf) ? buf : Buffer.from(buf)
+}
+
+export function zeroExtendBufToGivenSize(buf: Bytes, size: number = 32): Buffer {
+  if (buf.length === size) {
+    return convertToBufferIfNecessary(buf)
+  }
+
   if (buf.length > size) {
-    buf = buf.subarray(0, size)
+    return convertToBufferIfNecessary(buf.subarray(0, size))
   }
 
   const out = Buffer.alloc(size)
