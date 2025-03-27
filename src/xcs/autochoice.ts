0cbab3cc9bc2c674cd6b76c3941b46825acd2dd2
mmjee <root@git.maharshi.ninja>
2025-03-28 00:19:36 +0530

    Mediocre auto-choice algorithm

--- src/xcs/autochoice.ts
+++ src/xcs/autochoice.ts
@@ -1,12 +1,164 @@
-import { Aggregator } from "./iface";
-import { Currency, OmniversalChainID } from "../data";
-import { Bytes } from "../types";
+import { groupBy, orderBy } from "es-toolkit";
+import { bytesToBigInt, bytesToHex } from "viem";
+import { inspect as utilInspect } from 'util'
 
-export type Holding = {
-  chainID: OmniversalChainID
-  tokenAddress: Bytes
-  amount: bigint
+import { Aggregator, Quote, QuoteRequestExactInput, QuoteType } from "./iface";
+import { ChaindataMap, Currency, OmniversalChainID } from "../data";
+import { Bytes } from "../types";
+import { FixedFeeTuple } from "../proto/definition";
+
+function ezInspect (input: unknown) {
+  return utilInspect(input, {
+    depth: null,
+    colors: true
+  })
 }
 
-export async function autoSelectSources(holdings: Holding[], outputRequired: { currency: Currency, amount: bigint }, aggregators: Aggregator[]) {
+export type Holding = {
+  chainID: OmniversalChainID;
+  tokenAddress: Bytes;
+  amount: bigint;
+};
+
+export class AutoSelectionError extends Error {}
+
+export async function autoSelectSources(
+  holdings: Holding[],
+  outputRequired: { currency: Currency; amount: bigint },
+  aggregators: Aggregator[],
+  collectionFees: FixedFeeTuple[],
+) {
+  console.log('Holdings:', ezInspect(holdings))
+
+  const groupedByChainID = groupBy(holdings, (h) =>
+    bytesToHex(h.chainID.toBytes()),
+  );
+
+  const quoteRequests: {
+    req: QuoteRequestExactInput;
+    cfee: bigint;
+  }[] = [];
+  for (const holdings of Object.values(groupedByChainID)) {
+    const chain = ChaindataMap.get(holdings[0].chainID);
+    if (chain == null) {
+      throw new AutoSelectionError("Chain not found");
+    }
+    const correspondingCurrency = chain.Currencies.find(
+      (cur) => cur.currencyID === outputRequired.currency.currencyID,
+    );
+    if (correspondingCurrency == null) {
+      continue;
+    }
+    const cfeeTuple = collectionFees.find((cf) => {
+      return (
+        cf.universe === chain.Universe &&
+        Buffer.compare(cf.chainID, chain.ChainID32) === 0 &&
+        // output token is the CA one
+        Buffer.compare(cf.tokenAddress, correspondingCurrency.tokenAddress) === 0
+      );
+    });
+    const cfee = cfeeTuple != null ? bytesToBigInt(cfeeTuple.fee) : 0n;
+
+    for (const holding of holdings) {
+      quoteRequests.push({
+        req: {
+          type: QuoteType.ExactIn,
+          chain: chain.ChainID,
+          inputToken: holding.tokenAddress,
+          inputAmount: holding.amount,
+          outputToken: correspondingCurrency.tokenAddress,
+        },
+        // necessary for various purposes
+        cfee,
+      });
+    }
+  }
+
+  console.log('Quote Requests:', ezInspect(quoteRequests))
+  // TODO: simplify?
+  const _quoteOutputs = await Promise.all(
+    aggregators.map(async (agg) => {
+      const quotes = await agg.getQuotes(quoteRequests.map((qr) => qr.req));
+      const responses: ((typeof quoteRequests)[0] & {
+        quote: Quote;
+        agg: Aggregator;
+      })[] = new Array(quotes.length);
+      for (let i = 0; i < quotes.length; i++) {
+        responses[i] = {
+          ...quoteRequests[i],
+          quote: quotes[i]!,
+          agg,
+        };
+      }
+      return responses;
+    }),
+  );
+  const quoteOutputs = _quoteOutputs.flat();
+
+  // const groupedByChainID = groupBy(quoteOutputs, h => h.chainIDHex)
+  const orderedQuotes = orderBy(
+    quoteOutputs,
+    [
+      (quoteOut: (typeof quoteOutputs)[0]): unknown => quoteOut.cfee,
+      (quoteOut: (typeof quoteOutputs)[0]): unknown =>
+        quoteOut.quote.outputAmountMinimum, // once optimized for collections, we select the biggest asset we hold
+    ],
+    ["asc", "desc"],
+  );
+  console.log('Ordered:', ezInspect(quoteOutputs))
+
+  let remainder = outputRequired.amount;
+  const finalQuotes: ((typeof quoteOutputs)[0] & {
+    amt: bigint;
+  })[] = [];
+  for (const quote of orderedQuotes) {
+    if (remainder === 0n) {
+      break;
+    }
+    let amt = quote.quote.outputAmountMinimum;
+    if (remainder < amt) {
+      amt = remainder;
+    }
+    remainder -= amt;
+    finalQuotes.push({
+      ...quote,
+      amt,
+    });
+  }
+  if (remainder !== 0n) {
+    throw new AutoSelectionError("Failed to meet target!");
+  }
+
+  // we have to re-create the necessary quotes using QuoteExactOut
+  const groupedByAgg = Map.groupBy(finalQuotes, (qt) => qt.agg);
+  const response: typeof quoteOutputs = [];
+  await Promise.all(
+    Array.from(groupedByAgg.entries()).map(async ([agg, quotes]) => {
+      const outs = await agg.getQuotes(
+        quotes.map((quot) => ({
+          type: QuoteType.ExactOut,
+          chain: quot.req.chain,
+          inputToken: quot.req.inputToken,
+          outputToken: quot.req.outputToken,
+          outputAmount: quot.amt,
+        })),
+      );
+      for (let i = 0; i < outs.length; i++) {
+        const newQuote = outs[i]
+        if (newQuote == null) {
+          continue
+        }
+        const oldQuote = quotes[i]!
+
+        response.push({
+          agg,
+          cfee: oldQuote.cfee,
+          quote: newQuote,
+          req: oldQuote.req
+        })
+      }
+    }),
+  );
+
+  return response;
 }
