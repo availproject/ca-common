01a9f687ba41fa06e61a37cdaa0eedc641dec3b8
mmjee <root@git.maharshi.ninja>
2025-05-23 14:17:06 +0400

    DoubleCheck types and transactions (#35)
    * Added types for DoubleCheck
    
    * Added to the registry

--- src/proto/gen.sh
+++ src/proto/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --plugin="./node_modules/.bin/protoc-gen-ts_proto" --ts_proto_out="./src/proto" --proto_path="./src/proto" --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages,outputClientImpl=grpc-web" src/proto/*.proto
+protoc --plugin="./node_modules/.bin/protoc-gen-ts_proto" --ts_proto_out="./src/proto" --proto_path="./src/proto" --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages,oneof=unions-value,outputClientImpl=grpc-web" src/proto/*.proto
