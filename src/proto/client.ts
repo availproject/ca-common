2f97736847052c9c5389ad662e3bd9f4213331d1
Abhishek <abhishek.chaudhary.2008@gmail.com>
2025-12-12 15:32:38 +0400

    fix: use dynamic import

--- src/proto/client.ts
+++ src/proto/client.ts
@@ -1,13 +1,14 @@
 import { GrpcWebImpl } from "./grpc";
-import { NodeHttpTransport } from "@improbable-eng/grpc-web-node-http-transport";
 
-export const createGrpcWebImpl = (url: string) => {
+export const createGrpcWebImpl = async (url: string) => {
   const options: ConstructorParameters<typeof GrpcWebImpl>[1] = {};
 
   // eslint-disable-next-line no-constant-binary-expression, valid-typeof
   if (typeof window == undefined) {
-    options.transport = NodeHttpTransport();
+    const lib = await import("@improbable-eng/grpc-web-node-http-transport");
+    options.transport = lib.NodeHttpTransport();
   }
+
   const impl = new GrpcWebImpl(url, options);
   return impl;
 };
